c45a80a9d8c375f6670033cec581fd480f7e6380
liuhe <steelhe@qq.com>
2019-05-28 13:43:47 +0800

    测试协程

---
+++ src/main/kotlin/TestCoroutines.kt
@@ -0,0 +1,26 @@
+import kotlinx.coroutines.*
+
+//fun main(args: Array<String>) {
+//    println("Start")
+//
+//    // 启动一个协程
+//    GlobalScope.launch {
+//        delay(1000)
+//        println("Hello")
+//    }
+//
+//    Thread.sleep(2000) // 等待 2 秒钟
+//    println("Stop")
+//}
+
+fun main() {
+    println("Start")
+
+    GlobalScope.launch {
+        delay(1000)
+        println("Hello")
+    }
+
+    Thread.sleep(2000)
+    println("stop")
+}
